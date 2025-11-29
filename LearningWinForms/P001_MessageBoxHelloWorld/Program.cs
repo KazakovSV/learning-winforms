2cf94fab8cafe061b620931c2f8568e5303f6d69
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-29 23:57:40 +0300

    feat: add P001_MessageBoxHelloWorld project

---
+++ LearningWinForms/P001_MessageBoxHelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace P001_MessageBoxHelloWorld;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MessageBox.Show(
+            "Hello, World!",
+            "Message Box Window",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Exclamation);
+    }
+}
