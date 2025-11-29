e255458d0f02922a4c172ca52f4fb29b71803a05
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-30 00:16:10 +0300

    feat: add P004_ShowFormAndSleep project

---
+++ LearningWinForms/P004_ShowFormAndSleep/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+
+namespace P004_ShowFormAndSleep;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var form = new Form();
+        form.Show();
+
+        Thread.Sleep(2500);
+
+        form.Text = "My First Form";
+
+        Thread.Sleep(2500);
+    }
+}
