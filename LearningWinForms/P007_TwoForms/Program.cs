c0bfe8cb77a747df643dd50cd46f3172b67d415d
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-30 00:30:44 +0300

    feat: add P007_TwoForms project

---
+++ LearningWinForms/P007_TwoForms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace P007_TwoForms;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var form1 = new Form();
+        var form2 = new Form();
+
+        form1.Text = "Form passed to Run()";
+        form2.Text = "Second form";
+        form2.Show();
+
+        Application.Run(form1);
+
+        MessageBox.Show(
+            "Application.Run() has returned control back to Main(). Bye bye!",
+            "Two Forms");
+    }
+}
