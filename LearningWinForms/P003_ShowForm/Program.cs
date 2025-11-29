f69b718eb0a6042fe81087113399651b4f5397fc
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-30 00:11:37 +0300

    feat: add P003_ShowForm project

---
+++ LearningWinForms/P003_ShowForm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace P003_ShowForm;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var form = new Form();
+        form.Show();
+    }
+}
