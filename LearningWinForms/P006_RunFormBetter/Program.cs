cbf9d1c20be33486b1df04dfbceb20c2544ec0d4
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-30 00:23:42 +0300

    feat: add P006_RunFormBetter project

---
+++ LearningWinForms/P006_RunFormBetter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace P006_RunFormBetter;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var form = new Form();
+
+        form.Text = "My Very Own Form";
+
+        Application.Run(form);
+    }
+}
