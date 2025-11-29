d0adff0e37474466acf18e917194c69207796bc5
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-30 00:21:05 +0300

    feat: add P005_RunFormBadly project

---
+++ LearningWinForms/P005_RunFormBadly/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace P005_RunFormBadly;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var form = new Form();
+
+        form.Text = "Not a Good Idea...";
+        form.Visible = true;
+
+        Application.Run();
+    }
+}
