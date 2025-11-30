68b577ad19ae9c2affda34c427aeb9f81497dfa0
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-30 21:58:23 +0300

    feat: add P009_PaintEvent project

---
+++ LearningWinForms/P009_PaintEvent/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P009_PaintEvent;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var form = new Form();
+
+        form.Text = "Paint Event";
+        form.Paint += OnPaint;
+
+        Application.Run(form);
+    }
+
+    private static void OnPaint(object? sender, PaintEventArgs e)
+    {
+        var graphics = e.Graphics;
+        graphics.Clear(Color.Chocolate);
+        Console.WriteLine("Paint Event");
+    }
+}
