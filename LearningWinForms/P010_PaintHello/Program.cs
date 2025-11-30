b17f59dda5a52278f6f4fb20ecfa461c75ab8f19
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-30 23:42:44 +0300

    feat: add P010_PaintHello project

---
+++ LearningWinForms/P010_PaintHello/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P010_PaintHello;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var form = new Form
+        {
+            Text = "Paint Hello",
+            BackColor = Color.White,
+        };
+
+        form.Paint += OnPaint;
+
+        Application.Run(form);
+    }
+
+    private static void OnPaint(object? sender, PaintEventArgs e)
+    {
+        if (sender is Form form)
+        {
+            var graphics = e.Graphics;
+            graphics.DrawString("Hello, World!", form.Font, Brushes.Black, 0, 0);
+        }
+    }
+}
