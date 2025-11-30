cd1762433205802be42af1d26aa15656a04091bb
Stanislav Kazakov <mail@kazakovsv.com>
2025-11-30 21:37:11 +0300

    feat: add P008_FormProperties project

---
+++ LearningWinForms/P008_FormProperties/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P008_FormProperties;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var form = new Form();
+
+        form.Text = "Form Properties";
+        form.BackColor = Color.BlanchedAlmond;
+        form.Width *= 2;
+        form.Height /= 2;
+
+        form.FormBorderStyle = FormBorderStyle.FixedSingle;
+        form.MaximizeBox = false;
+        form.Cursor = Cursors.Hand;
+        form.StartPosition = FormStartPosition.CenterScreen;
+
+        Application.Run(form);
+    }
+}
