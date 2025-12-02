967054d2a37063a8cae1914fe4c8ae0c97291ca9
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-02 22:44:59 +0300

    feat: add P013_InheritTheForm project

---
+++ LearningWinForms/P013_InheritTheForm/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P013_InheritTheForm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var form = new InheritTheForm
+            {
+                Text = "Inherit The Form",
+                BackColor = Color.White
+            };
+
+            Application.Run(form);
+        }
+    }
+
+    internal class InheritTheForm : Form
+    {
+    }
+}
