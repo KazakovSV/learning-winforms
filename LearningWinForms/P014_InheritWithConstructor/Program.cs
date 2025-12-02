3dd01b156da00a90130e3a86052d4fd1f7d2969c
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-02 22:50:12 +0300

    feat: add P014_InheritWithConstructor project

---
+++ LearningWinForms/P014_InheritWithConstructor/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P014_InheritWithConstructor
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Application.Run(new InheritWithConstructor());
+        }
+    }
+
+    internal class InheritWithConstructor : Form
+    {
+        public InheritWithConstructor()
+        {
+            Text = "Inherit with Constructor";
+            BackColor = Color.White;
+        }
+    }
+}
