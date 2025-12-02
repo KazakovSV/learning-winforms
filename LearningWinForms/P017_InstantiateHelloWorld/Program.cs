3aff0b090fbe2b4bf31b18d12b4e173ef2249fcf
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-02 23:22:17 +0300

    feat: add P017_InstantiateHelloWorld project

---
+++ LearningWinForms/P017_InstantiateHelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using P015_HelloWorld;
+
+namespace P017_InstantiateHelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var form = new HelloWorld();
+
+            form.Text = "Instantiate " + form.Text;
+            form.Paint += OnPaint;
+
+            Application.Run(form);
+        }
+
+        private static void OnPaint(object? sender, PaintEventArgs e)
+        {
+            if (sender is Form form)
+            {
+                e.Graphics.DrawString("Hello from InstantiateHelloWorld!",
+                    form.Font, Brushes.Black, 0, 100);
+            }
+        }
+    }
+}
