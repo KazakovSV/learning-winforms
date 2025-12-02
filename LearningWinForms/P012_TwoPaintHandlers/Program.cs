ee975e5411dc673bd40d45091ed4b9c01d8941d0
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-02 22:39:02 +0300

    feat: add P012_TwoPaintHandlers project

---
+++ LearningWinForms/P012_TwoPaintHandlers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace P012_TwoPaintHandlers
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var form = new Form
+            {
+                Text = "Two Paint Handlers",
+                BackColor = Color.White
+            };
+
+            form.Paint += OnPaint1;
+            form.Paint += OnPaint2;
+
+            Application.Run(form);
+        }
+
+        private static void OnPaint1(object? sender, PaintEventArgs e)
+        {
+            if (sender is Form form)
+            {
+                e.Graphics.DrawString("First Paint Event Handler",
+                    form.Font, Brushes.Black, 0, 0);
+            }
+        }
+
+        private static void OnPaint2(object? sender, PaintEventArgs e)
+        {
+            if (sender is Form form)
+            {
+                e.Graphics.DrawString("Second Paint Event Handler",
+                    form.Font, Brushes.Black, 0, 100);
+            }
+        }
+    }
+}
