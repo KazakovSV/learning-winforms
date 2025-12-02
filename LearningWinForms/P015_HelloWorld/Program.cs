deba1e6fc6c220905454ed26b897d36110d5d275
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-02 22:56:01 +0300

    feat: add P015_HelloWorld project

---
+++ LearningWinForms/P015_HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P015_HelloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Application.Run(new HelloWorld());
+        }
+    }
+
+    internal class HelloWorld : Form
+    {
+        public HelloWorld()
+        {
+            Text = "Hello World";
+            BackColor = Color.White;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.DrawString("Hello, Windows Forms!", Font, Brushes.Black, 0, 0);
+        }
+    }
+}
