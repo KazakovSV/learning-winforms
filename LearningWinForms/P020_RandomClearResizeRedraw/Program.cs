c2c779aade7e16939982d5213858c43c3aa5f9b0
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-03 23:17:38 +0300

    feat: add P020_RandomClearResizeRedraw project

---
+++ LearningWinForms/P020_RandomClearResizeRedraw/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P020_RandomClearResizeRedraw;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Application.Run(new RandomClearResizeRedraw());
+    }
+}
+
+internal class RandomClearResizeRedraw : Form
+{
+    private readonly Random _random = new Random();
+
+    public RandomClearResizeRedraw()
+    {
+        Text = "Random Clear with ResizeRedraw";
+        ResizeRedraw = true;
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        var color = Color.FromArgb(_random.Next(256), _random.Next(256), _random.Next(256));
+        e.Graphics.Clear(color);
+    }
+}
