251eff3b207dc4e4d6fa800932e02564d4527e92
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-03 23:13:57 +0300

    feat: add P019_RandomClear project

---
+++ LearningWinForms/P019_RandomClear/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P019_RandomClear;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Application.Run(new RandomClear());
+    }
+}
+
+internal class RandomClear : Form
+{
+    private readonly Random _random = new Random();
+
+    public RandomClear()
+    {
+        Text = "Random Clear";
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        var color = Color.FromArgb(_random.Next(256), _random.Next(256), _random.Next(256));
+        e.Graphics.Clear(color);
+    }
+}
