eff81a1ca86b774fc7264d02cf383a543210c6cd
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-03 23:42:20 +0300

    feat: add P023_HelloCenteredMeasured project

---
+++ LearningWinForms/P023_HelloCenteredMeasured/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P023_HelloCenteredMeasured;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Application.Run(new HelloCenteredMeasured());
+    }
+}
+
+internal class HelloCenteredMeasured : Form
+{
+    public HelloCenteredMeasured()
+    {
+        Text = "Hello Centered Using MeasureString";
+        BackColor = SystemColors.Window;
+        ForeColor = SystemColors.WindowText;
+        ResizeRedraw = true;
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        var graphics = e.Graphics;
+        var brush = new SolidBrush(ForeColor);
+        var text = "Hello, World!";
+        var textSize = graphics.MeasureString(text, Font);
+
+        var textX = (ClientSize.Width - textSize.Width) / 2;
+        var textY = (ClientSize.Height - textSize.Height) / 2;
+
+        graphics.DrawString(text, Font, brush, textX, textY);
+    }
+}
