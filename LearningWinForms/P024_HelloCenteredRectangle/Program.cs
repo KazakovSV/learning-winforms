43acb7d27a22751ec46d3e213262fd923bdf13dd
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-04 21:28:14 +0300

    feat: add P024_HelloCenteredRectangle project

---
+++ LearningWinForms/P024_HelloCenteredRectangle/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P024_HelloCenteredRectangle;
+
+internal class HelloCenteredRectangle : Form
+{
+    static void Main(string[] args)
+    {
+        Application.Run(new HelloCenteredRectangle());
+    }
+
+    public HelloCenteredRectangle()
+    {
+        Text = "Hello Centered Using Rectangle";
+        BackColor = SystemColors.Window;
+        ForeColor = SystemColors.WindowText;
+        ResizeRedraw = true;
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        var format = new StringFormat
+        {
+            Alignment = StringAlignment.Center,
+            LineAlignment = StringAlignment.Center,
+        };
+
+        e.Graphics.DrawString("Hello, World!", Font, SystemBrushes.WindowText, ClientRectangle, format);
+    }
+}
