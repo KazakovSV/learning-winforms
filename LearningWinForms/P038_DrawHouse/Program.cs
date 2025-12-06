b3ed83bcc010a29628b22f489f9d8489c15b6e19
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-07 00:01:01 +0300

    feat: add P038_DrawHouse project

---
+++ LearningWinForms/P038_DrawHouse/Program.cs
@@ -0,0 +1,35 @@
+﻿using P035_PrintableForm;
+
+namespace P038_DrawHouse;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Application.Run(new DrawHouse());
+    }
+}
+
+public class DrawHouse : PrintableForm
+{
+    public DrawHouse()
+    {
+        Text = "Draw a House in One Line";
+    }
+
+    protected override void DoPage(Graphics graphics, Color color, int width, int height)
+    {
+        graphics.DrawLines(new Pen(color),
+        [
+            new Point( width / 4, 3 * height / 4),
+            new Point( width / 4, height / 2),
+            new Point( width / 2, height / 4),
+            new Point(3 * width / 4, height / 2),
+            new Point(3 * width / 4, 3 * height / 4),
+            new Point( width / 4, height / 2),
+            new Point(3 * width / 4, height / 2),
+            new Point( width / 4, 3 * height / 4),
+            new Point(3 * width / 4, 3 * height / 4)
+        ]);
+    }
+}
