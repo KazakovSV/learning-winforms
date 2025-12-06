07c0b8382df1dd7b0171d740fb81c85fe76979ed
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-06 23:56:42 +0300

    feat: add P037_BoxingTheClient project

---
+++ LearningWinForms/P037_BoxingTheClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using P035_PrintableForm;
+
+namespace P037_BoxingTheClient;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Application.Run(new BoxingTheClient());
+    }
+}
+
+public class BoxingTheClient : PrintableForm
+{
+    public BoxingTheClient()
+    {
+        Text = "Boxing The Client";
+    }
+
+    protected override void DoPage(Graphics graphics, Color color, int width, int height)
+    {
+        var points = new Point[]
+        {
+            new Point(0, 0),
+            new Point(width - 1, 0),
+            new Point(width - 1, height - 1),
+            new Point(0, height - 1),
+            new Point(0, 0)
+        };
+
+        graphics.DrawLines(new Pen(color), points);
+    }
+}
