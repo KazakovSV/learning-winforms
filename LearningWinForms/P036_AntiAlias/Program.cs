3c06ceb43debc0b1bb2b88a16f70cc3c69d21745
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-06 23:48:44 +0300

    feat: add P036_AntiAlias project

---
+++ LearningWinForms/P036_AntiAlias/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace P036_AntiAlias;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Application.Run(new AntiAlias());
+    }
+}
+
+public class AntiAlias : Form
+{
+    public AntiAlias()
+    {
+        Text = "Anti Alias Demo";
+        BackColor = SystemColors.Window;
+        ForeColor = SystemColors.WindowText;
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        var pen = new Pen(ForeColor);
+
+        e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+        e.Graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+        e.Graphics.DrawLine(pen, 2, 2, 18, 10);
+    }
+}
