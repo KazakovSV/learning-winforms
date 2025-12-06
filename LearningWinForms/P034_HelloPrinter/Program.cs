08584d7884027a129d7aa59f8093623961007d9a
Stanislav Kazakov <mail@kazakovsv.com>
2025-12-06 18:35:27 +0300

    feat: add P034_HelloPrinter project

---
+++ LearningWinForms/P034_HelloPrinter/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing.Printing;
+
+namespace P034_HelloPrinter;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Application.Run(new HelloPrinter());
+    }
+}
+
+internal class HelloPrinter : Form
+{
+    public HelloPrinter()
+    {
+        Text = "Hello Printer!";
+        BackColor = SystemColors.Window;
+        ForeColor = SystemColors.WindowText;
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        var format = new StringFormat
+        {
+            Alignment = StringAlignment.Center,
+            LineAlignment = StringAlignment.Center
+        };
+
+        e.Graphics.DrawString("Click to print", Font, SystemBrushes.WindowText, ClientRectangle, format);
+    }
+
+    protected override void OnClick(EventArgs e)
+    {
+        var document = new PrintDocument { DocumentName = Text };
+        document.PrintPage += OnPrintPage;
+        document.Print();
+    }
+
+    private void OnPrintPage(object sender, PrintPageEventArgs e)
+    {
+        if (e.Graphics != null)
+        {
+            e.Graphics.DrawString(Text, Font, Brushes.Black, 0, 0);
+
+            var textSize = e.Graphics.MeasureString(Text, Font);
+
+            e.Graphics.DrawLine(Pens.Black, textSize.ToPointF(), e.Graphics.VisibleClipBounds.Size.ToPointF());
+        }
+    }
+}
